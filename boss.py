325a3720348213deeb9967ac7d365a5b823c2731
m7md1337 <kingsncksa@gmail.com>
2021-09-30 13:32:48 -0400

    Add files via upload

---
+++ boss.py
@@ -0,0 +1,13 @@
+def cheche(x,y): # x mean number of loop , y mean number your enter ex: 7 
+  Center = round(y/2+0.5)
+  diff = abs(Center-x) # abs function conver nigatave to positive
+  space = abs((diff+diff+2)-y)
+  if x == 1 or x == y:
+    return " "*diff + "*" + " "*diff
+  else:
+    return " "*diff+"*"+" "*space+"*"+" "*diff
+ 
+num = int(input("enter num: "))
+for xx in range(1,num+1):
+  print(cheche(xx,num))
+
